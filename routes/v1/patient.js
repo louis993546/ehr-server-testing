f85abd9d2fdd27eb78b3c6d95325276399df7240
Louis <louis993546@gmail.com>
2015-09-14 14:20:22 +0800

    ADD: a temp JSON object of patient + checking

--- routes/v1/patient.js
+++ routes/v1/patient.js
@@ -3,7 +3,7 @@
  */
 var express = require('express');
 var router = express.Router();
-var ba = require('basic-auth')
+var ba = require('basic-auth');
 
 router.get('/search/', function (req, res) {
     var gender = req.query.gender;
@@ -12,8 +12,13 @@ router.get('/search/', function (req, res) {
 
 /* GET with patient id */
 router.get('/:id', function(req, res) {
+    var temp_patient = JSON.parse('{"id":"6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b","name":{"firstname":"Tsz Ho","middlename":"XXXX","lastname":"Tsai"},"birthday":{"year":1994,"month":9,"day":16},"gender":"male","race":"Mongolian","address":{"apartment":"A","floor":"99","village":"Sth","building":"Sth else","street":"sth street","province":"somewhere","country":"Cambodia"},"picture_id":"1024"}');
     var user = ba(req);
-    var message = 'username = ' + user.name + '\n' + 'password = ' + user.pass + '\n' + 'patient id = ' + req.params.id;
+    var message;
+    if (req.params.id == temp_patient.id) {
+        message = 'are you looking for me? \n' + JSON.stringify(temp_patient) + '\n';
+    }
+    message = message + 'username = ' + user.name + '\n' + 'password = ' + user.pass + '\n' + 'patient id = ' + req.params.id;
     res.send(message);
 });
 
