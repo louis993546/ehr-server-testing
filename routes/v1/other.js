d59d16184b88b16ba87211bebd30aec9207ca882
Louis <louis993546@gmail.com>
2015-10-06 20:20:46 +0800

    ADD: /other/country/ to get list of countries

--- routes/v1/other.js
+++ routes/v1/other.js
@@ -13,4 +13,14 @@ router.get('/apk/', function (req, res, next) {
     res.send('apk');
 });
 
+/**
+ * Send JSON Array of countries
+ */
+router.get('/country/', function (req, res, next) {
+    //TODO should I embed the JSON Object to this file or keep it separated?
+    res.send('[{"country_id":1,"english_name":"Hong Kong","phone_country_code":852},{"country_id":2,"english_name":"China","phone_country_code":86},{"country_id":3,"english_name":"Macau","phone_country_code":853},{"country_id":4,"english_name":"Cambodia","phone_country_code":855}]');
+});
+
+
+
 module.exports = router;
--- routes/v1/patient.js
+++ routes/v1/patient.js
@@ -12,7 +12,7 @@ router.get('/search/', function (req, res) {
     res.send('searching stuff: ' + gender);
 });
 
-/* GET with patient id */
+/* GET with patient id + basic auth */
 router.get('/:id', function(req, res) {
     var temp_patient = JSON.parse('{"id":"6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b","name":{"firstname":"Tsz Ho","middlename":"XXXX","lastname":"Tsai"},"birthday":{"year":1994,"month":9,"day":16},"gender":"male","race":"Mongolian","address":{"apartment":"A","floor":"99","village":"Sth","building":"Sth else","street":"sth street","province":"somewhere","country":"Cambodia"},"picture_id":"1024"}');
     var user = ba(req);
